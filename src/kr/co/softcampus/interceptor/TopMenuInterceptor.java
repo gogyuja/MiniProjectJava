74bf48085d77d7a41be32dcdef98c761a2d79e29
gogyu <gogyu@DESKTOP-KF14CO4>
2021-08-19 15:19:19 +0900

    인프런 / 윤재성의 만들면서 배우는 Spring MVC 5 JAVA 46강 까지

---
+++ src/kr/co/softcampus/interceptor/TopMenuInterceptor.java
@@ -0,0 +1,33 @@
+package kr.co.softcampus.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import kr.co.softcampus.beans.BoardInfoBean;
+import kr.co.softcampus.service.TopMenuService;
+
+public class TopMenuInterceptor implements HandlerInterceptor {
+	
+	private TopMenuService topMenuService;
+	
+	public TopMenuInterceptor(TopMenuService topMenuService) {
+		this.topMenuService=topMenuService;
+	}
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// TODO Auto-generated method stub
+		// 인터셉터에서는 자동주입 autowired를 통해 빈을 자동 주입 받을 수 없다.
+		// 그래서 인터셉터를 등록하는 쪽에서 빈을 주입받은 후 생성자로 넘겨서 작업해야한다.
+		// 그러면 ServletappContext에서 autowired로 생성한 다음 TopMenuInterceptor로 주소값을 받아 쓰겠다는 말이다
+		List<BoardInfoBean> topMenuList=topMenuService.getTopMenuList();
+		request.setAttribute("topMenuList", topMenuList);
+		return true;
+	}
+}
