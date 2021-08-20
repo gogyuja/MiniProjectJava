b3f33c835a9c43b3430345e24cdaf6c00b020801
gogyu <gogyu@DESKTOP-KF14CO4>
2021-08-20 15:29:02 +0900

    50강 회원가입 저장처리

--- src/kr/co/softcampus/mapper/UserMapper.java
+++ src/kr/co/softcampus/mapper/UserMapper.java
@@ -1,10 +1,16 @@
 package kr.co.softcampus.mapper;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
+
+import kr.co.softcampus.beans.UserBean;
 
 public interface UserMapper {
 	@Select("SELECT user_name "+
 			"FROM user_table "+
 			"WHERE user_id= #{user_id}")
 	String checkUserIdExist(String user_id);
+
+	@Insert("insert into user_table(user_name,user_id,user_pw) "+"values(#{user_name},#{user_id},#{user_pw})")
+	void addUserInfo(UserBean joinUserBean);
 }
