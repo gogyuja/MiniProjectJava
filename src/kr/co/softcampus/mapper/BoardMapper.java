3e71bfe6157be530748fcaff0bb6d2b58dcf62d7
gogyu <gogyu@DESKTOP-KF14CO4>
2021-09-07 21:58:30 +0900

    57강 글 목록 구현하기

--- src/kr/co/softcampus/mapper/BoardMapper.java
+++ src/kr/co/softcampus/mapper/BoardMapper.java
@@ -1,6 +1,9 @@
 package kr.co.softcampus.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
 
 import kr.co.softcampus.beans.ContentBean;
 
@@ -11,5 +14,16 @@ public interface BoardMapper {
 				"values(#{content_subject},#{content_text},#{content_file},"+
 				"#{content_writer_idx}, #{content_board_idx}, sysdate())")
 		void addContentInfo(ContentBean writeContentBean);
+		
+		@Select("SELECT board_info_name "+
+				"FROM board_info_table "+
+				"WHERE board_info_idx=#{board_info_idx}")
+		String getBoardInfoName(int board_info_idx);
+		
+		@Select("SELECT a1.content_idx, a1.content_subject, a2.user_name content_writer_name, "+
+				"date_format(a1.content_date,'%Y-%m-%d') content_date " + 
+				"FROM content_table a1, user_table a2 " + 
+				"WHERE a1.content_writer_idx=a2.user_idx AND a1.content_board_idx=#{board_info_idx} ORDER BY a1.content_idx DESC")
+		List<ContentBean> getContentList(int board_info_idx);
 }
 
